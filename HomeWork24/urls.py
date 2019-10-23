d731be788995af5666fdca3f564a11fd0a54a14e
Aniri2013 <aniri-13.rodina@yandex.ru>
2019-10-23 18:26:58 +0300

    comm

--- HomeWork24/urls.py
+++ HomeWork24/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
-    url(r'^app1/', include('App1.urls')),
+    url(r'app1/', include('App1.urls')),
     url(r'^app2/', include('App2.urls')),
     url(r'^app3/', include('App3.urls')),
 ]
